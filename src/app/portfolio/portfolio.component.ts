1aa7213785951d73b802c2ae9b73ccec2d53f750
Ansony Rojas <ansonyrojas2015@gmail.com>
2024-12-15 17:08:10 -0400

    Actualizacion del portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,10 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
-
+  screenWidth: number = 0;
+  screenHeight: number = 0;
   constructor() { }
 
   ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+
+    // Si deseas que se actualice en tiempo real cuando cambie la resolución
+    window.addEventListener('resize', () => {
+      this.screenWidth = window.innerWidth;
+      this.screenHeight = window.innerHeight;
+    });
   }
 
 }
